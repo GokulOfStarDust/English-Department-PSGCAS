cb13979ff76978ade85ff56a96304062ca8d3eb1
stardust <gokulgopalan18@gmail.com>
2025-01-01 22:09:36 +0530

    bug rectify

--- src/pages/EventData.jsx
+++ src/pages/EventData.jsx
@@ -2,8 +2,8 @@ const eventData = {
   //array of upcoming event's objects
   upcomingEvents: [
     {
-      id: "aTailOfThreeQuizzes",
-      name: "A Tail of Three Quizzes",
+      id: "aTaleOfThreeQuizzes",
+      name: "A Tale of Three Quizzes",
       time: "09:00 a.m - 01:00 p.m",
       date: "6 JAN 2025 - 8 JAN 2025",
       location: "Vaigai Hall, Q-Block, PSG College of Arts & Science",
