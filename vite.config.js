acb1ed4839a59882869aa3a366d02d4fd117d357
stardust <gokulgopalan18@gmail.com>
2025-01-13 00:05:06 +0530

    fixed analytics bug

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      external: ['@vercel/analytics/react'], // Externalize only if needed
-      output: {
-        globals: {
-          '@vercel/analytics/react': 'VercelAnalytics', // Define global if using external
-        },
-      },
-    },
-  },
+  base: "/",
+  
 });
