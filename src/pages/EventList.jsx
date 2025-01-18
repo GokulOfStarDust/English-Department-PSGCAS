1f8cc6acbaf28b2216dc2a79be7664d579629a42
stardust <gokulgopalan18@gmail.com>
2025-01-18 19:42:54 +0530

    edited keywords in meta tag

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -26,6 +26,9 @@ export default function EventList() {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5, delay: 0.1 }}
         className={`${loadingState ? "" : "hidden"} fixed flex justify-center items-center w-full h-lvh z-[100] inset-0 bg-event-card-black`}
+        role="progressbar"
+        aria-live="assertive"
+        aria-label={loadingState ? "Loading events, please wait." : "Events loaded."}
       >
         <motion.div
           initial={{ opacity: 0 }}
@@ -42,16 +45,18 @@ export default function EventList() {
         transition={{ duration: 0.2, delay: 0.1 }}
         className="flex flex-row flex-wrap items-center justify-center  bg-cover bg-center bg-fixed space-y-2 pb-32 scroll-smooth"
         style={{ backgroundImage: `url('/bg.webp')` }}
+        role="list"
       >
         <div
           className="flex items-center justify-center bg-[url('/eventsTitleBg.png')] bg-no-repeat bg-cover bg-center w-[90svw] min-h-[60px] md:min-h-[80px] my-16 !mb-[15svh]
                             font-josefin text-event-card-black font-bold text-[1.7rem] md:text-4xl tracking-[3.8px]
                             rounded-3xl outline outline-1 outline-black"
+          role="banner"
         >
           UPCOMING EVENTS
         </div>
         {eventData.upcomingEvents.length == 0 ? (
-          <li className="font-bebas text-4xl md:text-5xl text-[#232323] font-medium text-center p-6">
+          <li className="font-bebas text-4xl md:text-5xl text-[#232323] font-medium text-center p-6" role="listitem">
             ALL THE WORLD'S A STAGE, BUT NO EVENTS ARE PLAYING RIGHT NOW—
             <br />
             STAY TUNED FOR THE NEXT ACT!
@@ -59,7 +64,7 @@ export default function EventList() {
         ) : (
           eventData.upcomingEvents.map((event) => {
             return (
-              <li key={event.id} className="list-none">
+              <li key={event.id} className="list-none" role="listitem">
                 <div
                   onClick={() => {
                     navigate(`/events/${event.id}`);
@@ -70,8 +75,9 @@ export default function EventList() {
                       <img
                         className={`w-[85svw] md:w-[75svw] h-[45svh] md:h-[45svh] object-cover object-center rounded-xl text-white`}
                         src={event.imageUrl}
-                        alt="Poster of 'A Tale of Three Quizzes'"
+                        alt={`Poster of ${event.name}`}
                         onLoad={imageLoadHandle}
+                        loading="lazy"
                       />
                     </motion.div>
                     <motion.div className="font-josefin text-white font-bold italic text-3xl xl:text-4xl">
@@ -90,7 +96,7 @@ export default function EventList() {
                           </div>
                           <img
                             src="/separatorLine.png"
-                            alt="line"
+                            alt="Separator line"
                             className="hidden divide-breakpoint:block object-contain size-7 md-4 mr-3 "
                           />
                           <div className="flex flex-row items-center p-4 pl-0 pt-0 divide-breakpoint:pt-4  space-x-2">
@@ -105,7 +111,7 @@ export default function EventList() {
                         <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                           <img
                             src="/locationPin-event-icon.png"
-                            alt="location pin icon to represent event location"
+                            alt="Location pin icon to represent event location"
                             className="object-contain size-5"
                           />
                           <p>{event.location}</p>
@@ -114,7 +120,7 @@ export default function EventList() {
                           <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                             <img
                               src="/cash-pool-logo.png"
-                              alt="cash pool icon to represent event cash prize"
+                              alt="Cash pool icon to represent event cash prize"
                               className="object-contain size-5"
                             />
                             <p>{event.cashPrizeInfo}</p>
@@ -123,7 +129,7 @@ export default function EventList() {
                         <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                           <img
                             src="/register-icon.png"
-                            alt="register icon to represent on-spot registration"
+                            alt="Register icon to represent on-spot registration"
                             className="object-contain size-6 opacity-70"
                           />
                           <p>On-spot registration is available.</p>
@@ -131,10 +137,10 @@ export default function EventList() {
                         <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                           <img
                             src="/cash-pool-logo.png"
-                            alt="register icon to represent on-spot registration"
+                            alt="Register icon to represent registration fee"
                             className="object-contain size-6 opacity-70"
                           />
-                          <p>Registeration Fee: {event.registerationFeeInfo}</p>
+                          <p>Registration Fee: {event.registerationFeeInfo}</p>
                         </div>
                       </div>
                     </motion.div>
@@ -143,6 +149,7 @@ export default function EventList() {
                       onClick={() => {
                         navigate(`/events/${event.id}`);
                       }}
+                      aria-label={`View competitions for ${event.name}`}
                     >
                       Click to view the competitions!
                     </div>
@@ -154,8 +161,6 @@ export default function EventList() {
         )}
       </motion.ul>
 
-      {/*worst way (increase redunduncy) used to display past events */}
-
       <motion.ul
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -163,24 +168,27 @@ export default function EventList() {
         transition={{ duration: 0.2, delay: 0.1 }}
         className="flex flex-row flex-wrap items-center justify-center  bg-cover bg-center bg-fixed space-y-2 pb-32 scroll-smooth"
         style={{ backgroundImage: `url('/bg.webp')` }}
+        role="list"
       >
         <div
           className="flex items-center justify-center bg-[url('/eventsTitleBg.png')] bg-no-repeat bg-cover bg-center w-[90svw] min-h-[60px] md:min-h-[80px] my-16 mb-[19svh]
                             font-josefin text-event-card-black font-bold text-[1.7rem] md:text-4xl tracking-[3.8px]
                             rounded-3xl outline outline-1 outline-black"
+          role="banner"
         >
           PAST EVENTS
         </div>
         {eventData.pastEvents.map((event) => {
           return (
-            <li key={event.id} className="list-none">
+            <li key={event.id} className="list-none" role="listitem">
               <div>
                 <motion.div className="flex flex-col bg-event-card-black w-[88svw] md:w-[65svw] p-7 pt-7 space-y-5 rounded-3xl ">
                   <motion.div className="flex flex-row justify-center">
                     <img
                       className={`w-[85svw] md:w-[75svw] h-[45svh] md:h-[45svh] object-cover object-center rounded-xl text-white`}
                       src={event.imageUrl}
-                      alt="Poster of 'A Tale of Three Quizzes'"
+                      alt={`Poster of ${event.name}`}
+                      loading="lazy"
                     />
                   </motion.div>
                   <motion.div className="font-josefin text-white font-bold italic text-3xl xl:text-4xl">
@@ -199,7 +207,7 @@ export default function EventList() {
                         </div>
                         <img
                           src="/separatorLine.png"
-                          alt="line"
+                          alt="Separator line"
                           className="hidden divide-breakpoint:block object-contain size-7 md-4 mr-3 "
                         />
                         <div className="flex flex-row items-center p-4 pl-0 pt-0 divide-breakpoint:pt-4  space-x-2">
@@ -214,7 +222,7 @@ export default function EventList() {
                       <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                         <img
                           src="/locationPin-event-icon.png"
-                          alt="location pin icon to represent event location"
+                          alt="Location pin icon to represent event location"
                           className="object-contain size-5"
                         />
                         <p>{event.location}</p>
@@ -222,7 +230,7 @@ export default function EventList() {
                       <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                         <img
                           src="/cash-pool-logo.png"
-                          alt="cash pool icon to represent event cash prize"
+                          alt="Cash pool icon to represent event cash prize"
                           className="object-contain size-5"
                         />
                         <p>{event.cashPrizeInfo}</p>
@@ -230,7 +238,7 @@ export default function EventList() {
                       <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                         <img
                           src="/register-icon.png"
-                          alt="register icon to represent on-spot registration"
+                          alt="Register icon to represent on-spot registration"
                           className="object-contain size-6 opacity-70"
                         />
                         <p>On-spot registration is available.</p>
@@ -238,10 +246,10 @@ export default function EventList() {
                       <div className="flex flex-row items-center p-4 pl-0 pt-0 space-x-2">
                         <img
                           src="/cash-pool-logo.png"
-                          alt="register icon to represent on-spot registration"
+                          alt="Register icon to represent registration fee"
                           className="object-contain size-6 opacity-70"
                         />
-                        <p>Registeration Fee: {event.registerationFeeInfo}</p>
+                        <p>Registration Fee: {event.registerationFeeInfo}</p>
                       </div>
                     </div>
                   </motion.div>
@@ -250,6 +258,7 @@ export default function EventList() {
                     onClick={() => {
                       navigate(`/events/${event.id}`);
                     }}
+                    aria-label={`View details for ${event.name}`}
                   >
                     Click for More!
                   </div>
